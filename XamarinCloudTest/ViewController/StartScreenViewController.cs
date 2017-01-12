b19627f2f260597334b447795cafa6a4e9c3fbec
Diego José Fernández Hernández <diego@diegofer.com>
2017-01-12 09:52:29 +0100

    Creacion de proyecto.

---
+++ XamarinCloudTest/ViewController/StartScreenViewController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinCloudTest
+{
+	public partial class StartScreenViewController : UIViewController
+	{
+		public StartScreenViewController() : base(nameof(StartScreenViewController), null)
+		{
+			
+		}
+	}
+}
+
